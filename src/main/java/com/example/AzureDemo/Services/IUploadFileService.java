6765340ff8a0006ebebdf073bf3abdb059d60ffc
Barzu-Andreea <79249829+Barzu-Andreea@users.noreply.github.com>
2023-05-07 20:36:31 +0300

    Add files via upload

---
+++ src/main/java/com/example/AzureDemo/Services/IUploadFileService.java
@@ -0,0 +1,7 @@
+package com.example.AzureDemo.Services;
+
+import com.example.AzureDemo.Entities.UploadFileDto;
+
+public interface IUploadFileService {
+    public String uploadFileAzure (UploadFileDto uploadFileDto);
+}
