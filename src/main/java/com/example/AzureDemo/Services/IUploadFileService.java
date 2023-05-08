4b0b247dbb43e4c84da4cf6972208b21f1d892b8
Barzu-Andreea <79249829+Barzu-Andreea@users.noreply.github.com>
2023-05-08 19:39:15 +0300

    Delete src/main/java/com/example/AzureDemo/Services directory

--- src/main/java/com/example/AzureDemo/Services/IUploadFileService.java
+++
@@ -1,7 +0,0 @@
-package com.example.AzureDemo.Services;
-
-import com.example.AzureDemo.Entities.UploadFileDto;
-
-public interface IUploadFileService {
-    public String uploadFileAzure (UploadFileDto uploadFileDto);
-}
